34fde13ad4a24da1fc626e0f2a25485abcce29e6
Andres Eduardo <andrucar25@gmail.com>
2024-11-25 19:59:33 -0500

    Auth and User microservices finished

--- 04-agendamiento/01-auth/src/module/medic/domain/exceptions/id.exceptions.ts
+++
@@ -1,8 +0,0 @@
-export class IdException extends Error {
-  constructor() {
-    super();
-    this.name = "IdException";
-    this.message = "Invalid id";
-    Object.setPrototypeOf(this, IdException.prototype);
-  }
-}
